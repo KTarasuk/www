136a4b5c282c8e7e9f41a155ec23d78ac9e1f40d
Kirill Tarasuk <131031883+KTarasuk@users.noreply.github.com>
2024-06-01 12:51:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-console.log('Start program'); // Start program
+console.log('Start program');
 
-const widthSite = window.screen.width;
+const widthWebSite = window.screen.width; // Получаем ширину экрана
+const body = document.getElementById("bodyWebSite"); // Получаем body
+console.log(widthWebSite);
+switch (widthWebSite) { // Задаем размер шрифта по размеру экрана
+    case 1440:
+        console.log("Width | Active fontSize 20px");
+        body.style.fontSize = '20px';
+        break;
+    case 2560:
+        console.log("Width | Active fontSize 24px");    
+        body.style.fontSize = '24px';
+        break;
+    default:
+        break;
+}
 
 // === MenuBarMobile === \\
 const menuBar = document.getElementById('menuListMobile');
 
-if (widthSite <= 600){
-    console.info('Mobile | MenuBar Active');
-    function menuBarOpen(){
-        menuBar.style.fontSize = '100%';
-        menuBar.style.width = '100%';
-        menuBar.style.paddingTop = '25%';
-        menuBar.style.transition = '0.3s';
-        console.log('MenuBar | Open');
-    }
-    function menuBarClose(){
-        menuBar.style.width = "0%";
-        menuBar.style.fontSize = '0%';
-        console.log('MenuBar | Close');
-    }
-} else { console.log('Mobile | MenuBar not active'); }
+function menuBarOpen(){
+    menuBar.style.fontSize = '100%';
+    menuBar.style.width = '100%';
+    menuBar.style.paddingTop = '25%';
+    menuBar.style.transition = '0.35s';
+    console.log('MenuBar | Open');
+}
+function menuBarClose(){
+    menuBar.style.width = "0%";
+    menuBar.style.fontSize = '0%';
+    console.log('MenuBar | Close');
+}
 
 
 // === Info Project === \\
 
-let infoProject = document.getElementById('iP');
-let projectOne = document.getElementById('projectOne');
-let projectTwo = document.getElementById('projectTwo');
+const infoProject = document.getElementById('iP');
+const projectOne = document.getElementById('projectOne');
+const projectTwo = document.getElementById('projectTwo');
 
 function closeInfoProject() {
     infoProject.style.width = '0%';
@@ -42,12 +53,12 @@ function openInfoProject() {
 function openProjectOne(){
     projectOne.style.fontSize = '100%';
     projectOne.style.transition = '0.5s';
-    console.log('Project | Open One Project');
+    console.log('Projects | Open One Project');
 }
 function openProjectTwo(){
     projectTwo.style.fontSize = '100%';
     projectTwo.style.transition = '0.5s';
-    console.log('Project | Open Two Project');
+    console.log('Projects | Open Two Project');
 }
 
 function openIPPO(){
